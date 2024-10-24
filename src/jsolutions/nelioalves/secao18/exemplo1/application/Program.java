8abe89f93502f6942be1f7c1027848c6b54ce83f
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2024-10-23 21:51:58 -0300

    Expressão lambda in line

--- src/jsolutions/nelioalves/secao18/exemplo1/application/Program.java
+++ src/jsolutions/nelioalves/secao18/exemplo1/application/Program.java
@@ -2,7 +2,6 @@ package jsolutions.nelioalves.secao18.exemplo1.application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 import jsolutions.nelioalves.secao18.exemplo1.entities.Product;
 
@@ -16,8 +15,7 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 
-		Predicate<Product> pred = p -> p.getPrice() >= 900.00;
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= 900.00);
 
 		for (Product p : list) {
 			System.out.println(p);
